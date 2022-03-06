7b1f68a4ed65e518ff61fc4367aa7118476c03b2
indegser <indegser@gmail.com>
2022-03-06 22:05:49 +0900

    Add stitches

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { styled } from "../src/stitches.config";
 
 function Page() {
   const {
@@ -16,8 +17,18 @@ function Page() {
       }}
     >
       <h1 style={{ position: "absolute" }}>{text}</h1>
+      <Background src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-blue-202009_FMT_WHH?wid=1000&hei=1000&fmt=jpeg&qlt=95&.v=1599672435000" />
     </div>
   );
 }
+
+const Background = styled("img", {
+  width: "100vw",
+  height: "100vh",
+  objectFit: "cover",
+  objectPosition: "center",
+  position: "fixed",
+  zIndex: -1,
+});
 
 export default Page;
